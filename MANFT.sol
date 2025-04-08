6846335302ebb117a4ed954c54b338eaad3a94cf
isacpxc <isacpxc@gmail.com>
2025-04-08 19:34:15 -0300

    feat: changeTokenUri function | fix: removed unecessary features

--- MANFT.sol
+++ MANFT.sol
@@ -6,40 +6,25 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-interface MATK{
-    function myBalance(address) external returns(uint256);
-}
-
 contract MyArtNFT is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
     address private matk_contract_address;
     mapping (uint256 => uint256) private NFTprice;
 
     event e_minted(address indexed owner, uint256 toke_id);
-    
-    modifier realId(uint id) {
-        require(id <= _nextTokenId, "item does not exist");
-        _;
-    }
+    event e_nft_transfer(uint _id, string _uri);
 
     constructor(address initialOwner) ERC721("MyArtNFT", "MATNFT") Ownable(initialOwner){
 
     }
 
-    function getNFTprice(uint id) private view realId(id) returns(uint){
-        return NFTprice[id];
-    }
-
+    
     function set_matk_address(address addr) public onlyOwner {
         matk_contract_address = addr;
     }
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://";
-    }
-
-    function balance_matk(address owner) public onlyOwner returns(uint256) {
-        return MATK(matk_contract_address).myBalance(owner);
     }
 
     function safeMint(address to, string memory uri) public onlyOwner returns (uint256) {
@@ -56,6 +41,13 @@ contract MyArtNFT is ERC721, ERC721URIStorage, Ownable {
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+
+    function changeTokenUri(address currentOwner, uint id, string memory uri) public {
+        require(msg.sender == matk_contract_address, "not authorized");
+        require(currentOwner == ownerOf(id), "You're not the owner of this NFT.");
+        _setTokenURI(id, uri);
+        emit e_nft_transfer(id,uri);
     }
 
 }
