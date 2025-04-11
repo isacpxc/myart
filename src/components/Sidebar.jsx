16f39f125917bd4eb8fbcb51ce3829ac0730f481
isacpxc <isacpxc@gmail.com>
2025-04-11 17:01:35 -0300

    fix: pure css is better than tailwind

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,40 @@
+/* eslint-disable no-unused-vars */
 import { FaHome } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { CiCircleQuestion } from "react-icons/ci";
 import { FaGear } from "react-icons/fa6";
 import { NavLink } from "react-router";
+import "./sidebar.css"
 
 function Sidebar() {
     const iconSize = 30;
     return (
-        <nav className="fixed flex justify-between top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
-            <div className="">
-                <NavLink to="/" className="!mb-10">
-                    <SideBarIcon icon={<FaHome size={iconSize} />} text="Home" mb="!mb-5" mt="!mt-2" />
+        <nav className="sidebar">
+            <div className="sidebar-section">
+                <NavLink to="/">
+                    <SideBarIcon icon={<FaHome /*size={iconSize}*/ />} text="Home" top="10px" />
                 </NavLink>
                 <NavLink to="/profile">
-                    <SideBarIcon icon={<CgProfile size={iconSize} />} text="Profile" mb="!mb-0" mt="" />
+                    <SideBarIcon icon={<CgProfile /*size={iconSize}*/ />} text="Profile" />
                 </NavLink>
             </div>
-            <div className="">
+            <div className="sidebar-section">
                 <NavLink to="/settings">
-                    <SideBarIcon icon={<FaGear size={iconSize} />} text="Settings" mb="!mb-5" mt="" />
+                    <SideBarIcon icon={<FaGear /*size={iconSize}*/ />} text="Settings" />
                 </NavLink>
                 <NavLink to="/help">
-                    <SideBarIcon icon={<CiCircleQuestion size={iconSize} />} text="Help" mb="!mb-2" mt="" />
+                    <SideBarIcon icon={<CiCircleQuestion /*size={iconSize}*/ />} text="Help" />
                 </NavLink>
             </div>
         </nav>
     );
 }
 
-const SideBarIcon = ({ icon, text, mb, mt }) => (
-    <div className={"sidebar-icon group " + mb + " " + mt} >
+const SideBarIcon = ({ icon, text, top }) => (
+    <div className={"sidebar-icon"} style={{ marginTop: top }}>
         {icon}
 
-        <span className="sidebar-tooltip group-hover:scale-150">
+        <span className="subtitle-blink">
             {text}
         </span>
     </div>
