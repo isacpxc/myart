1c70e437195033e6e361a720678f0a99b5e92342
isacpxc <isacpxc@gmail.com>
2025-04-12 21:00:58 -0300

    feat: handle connection

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,23 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import {connectMM} from "./services/metaConnection"
+import { formatEther } from "ethers";
 
 function Profile() {
     const [connected, setConnected] = useState(0);
+
+    useEffect(()=>{},[connected]); // watching connection
+
+    const handleConnection = async ()=>{
+        const [provider, signer] = await connectMM();
+        const accSigner = await provider.getSigner();
+            // // testes
+            // console.log("numero bloco: ",await provider.getBlockNumber());
+            // console.log(rpcSigner.address);
+            // const balance = await provider.getBalance(rpcSigner.address)
+            // console.log("balance: ",formatEther(balance));
+            // console.log("tx count: ", await provider.getTransactionCount(rpcSigner.address));
+            // // testes fim
+            return accSigner;
+    }
 
     const NotConnectedProfile = () => {
         return (
             <>
                 <span>Not Connected</span>
                 <br />
-                <button onClick={()=>{
-                    connectMM();
+                <button onClick={async ()=>{
+                    handleConnection()
+                    .then( (res) => {
+                        console.log("entrou");
+                        console.log(res);
+                        localStorage.setItem("conn", JSON.stringify(res));
+                        setConnected(1);
+                    })
+                    .catch(err => {console.log("ERROR TRYING CONNECTION: ",err)})
                 }}>Login With Metamask</button>
-                <br />
-                <button onClick={() => {
-                    setConnected(!connected);
-                }}>changeState</button>
             </>
         );
     }
@@ -25,34 +44,37 @@ function Profile() {
     const ConnectedProfile = () => {
         return (
             <>
-                <span>Address: </span><input type="text" name="" id="" disabled />
+                <span>Address: </span><input type="text" placeholder={JSON.parse(localStorage.getItem('conn')).address} name="" id="" disabled />
                 <br />
-                <span>balance:</span> <input type="text" disabled />
+                <span>balance: </span><span id="TK-amount">?</span><span> MATK</span>
                 <button onClick={() => { updateBalance(3) }}>Refresh Balance</button>
                 <br />
                 <span>My Collection:</span> <button onClick={() => { alert('work') }}>Add</button>
                 <br />
+                <button onClick={()=>{
+
+                }}>logout</button>
                 <div className="hold-test-blocks">
                     {/* <div className="test-block"></div> */}
                 </div>
-                <button onClick={() => {
-                    setConnected(!connected);
-                }}>changeState</button>
             </>
         );
     } // end of ConnectedProfile
 
-    const updateBalance = (par) => {
-        alert('work' + par);
+
+    const ConnComponent = () => {
+        if (localStorage.conn == undefined) {
+            return <Main page={<NotConnectedProfile />} />;
+        } else {
+            return <Main page={<ConnectedProfile />} />;   
+        }
     }
 
 
     return (
         <>
-
             <Sidebar />
-            {connected ? <Main page={<ConnectedProfile />} /> : <Main page={<NotConnectedProfile />} />}
-
+            <ConnComponent/>            
         </>
     );
 }
