fbd218ba653af22d25b0b0f4c495eebfeca22a19
isacpxc <isacpxc@gmail.com>
2025-04-13 16:11:39 -0300

    feat: properly refresh balance

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import {connectMM} from "./services/metaConnection"
+import * as handleTx from "./services/handleTx"
 import { formatEther } from "ethers";
+import { createContractTK } from "./contracts/abi";
 
 function Profile() {
     const [connected, setConnected] = useState(0);
+    const [currentContract, setcurrentContract] = useState(null);
+    const [balance, setBalance] = useState("?");
 
     useEffect(()=>{},[connected]); // watching connection
+    useEffect(()=>{
+        if (localStorage.balance) setBalance(localStorage.balance);
+    },[]);
+
+    const handleLogout = () => {
+        localStorage.removeItem("conn");
+    }
 
     const handleConnection = async ()=>{
         const [provider, signer] = await connectMM();
@@ -29,7 +40,7 @@ function Profile() {
                 <br />
                 <button onClick={async ()=>{
                     handleConnection()
-                    .then( (res) => {
+                    .then( async (res) => {
                         console.log("entrou");
                         console.log(res);
                         localStorage.setItem("conn", JSON.stringify(res));
@@ -46,14 +57,30 @@ function Profile() {
             <>
                 <span>Address: </span><input type="text" placeholder={JSON.parse(localStorage.getItem('conn')).address} name="" id="" disabled />
                 <br />
-                <span>balance: </span><span id="TK-amount">?</span><span> MATK</span>
-                <button onClick={() => { updateBalance(3) }}>Refresh Balance</button>
+                <span>balance: </span><span onLoad={(item)=>{
+                    if (localStorage.balance) setBalance(localStorage.balance);
+                }}></span>{balance}<span> MATK</span>
+                <button onClick={async () => { 
+                    const addressAcc = String(JSON.parse(localStorage.conn).address);
+                    const contractTK = await createContractTK(await connectMM());
+                    console.log(contractTK);
+                    handleTx.getBalance(contractTK,addressAcc)
+                    .then((res)=>{
+                        localStorage.setItem("balance",res);
+                        setBalance(res);
+                    })
+                    .catch(err => +console.log("ERROR DURING TX: ", err))
+                 }}>Refresh Balance</button>
                 <br />
                 <span>My Collection:</span> <button onClick={() => { alert('work') }}>Add</button>
                 <br />
-                <button onClick={()=>{
-
+                <button onClick={async ()=>{
+                    () =>{setContracts(null)}
+                    handleLogout();
+                    setConnected(0);
                 }}>logout</button>
+                <br />
+                {/* <button onClick={()=>{showtest("cuzas")}}>test</button> */}
                 <div className="hold-test-blocks">
                     {/* <div className="test-block"></div> */}
                 </div>
