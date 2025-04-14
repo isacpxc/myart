bc59d228d86349c135d6b7f8f028fd209ab28478
isacpxc <isacpxc@gmail.com>
2025-04-14 15:34:17 -0300

    feat: new functions calls

--- src/services/handleTx.js
+++ src/services/handleTx.js
@@ -2,3 +2,13 @@ export const getBalance = async (contract, address) => {
   const balance = await contract.myBalance(address);
   return balance;
 };
+
+export const mintTK = async (contract, address, value) => {
+  const result = await contract.mint_MATK().send(value);
+  return result;
+};
+
+export const minNFT = async (contract, to, uri) => {
+  const result = await contract.safeMint(to, uri);
+  return result;
+};
