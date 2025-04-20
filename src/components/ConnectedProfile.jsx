f37640c709eb3c216f67e8d64ed140ee42742c57
isacpxc <isacpxc@gmail.com>
2025-04-20 17:43:55 -0300

    refactor

--- src/components/ConnectedProfile.jsx
+++ src/components/ConnectedProfile.jsx
@@ -35,6 +35,40 @@ export default function ConnectedProfile ({setConnected}) {
     setNft(hold);
   }
 
+  const handleRefreshBalance = async ()=> {
+    const addressAcc = String(JSON.parse(localStorage.conn).address);
+    const contractTK = await createContractTK(await connectMM());
+    console.log(contractTK);
+    handleTx.getBalance(contractTK,addressAcc)
+    .then((res)=>{
+        localStorage.setItem("balance",res);
+        console.log("successful query");
+        setBalance(res);
+    })
+    .catch(err => +console.log("ERROR DURING TX: ", err))
+  }
+
+  const handleMintTK = async ()=>{
+    const signer = (await connectMM())[1];
+    const contractTK = await createContractTK([signer]);
+    console.log(signer);
+    console.log(contractTK);
+    const tx = await handleTx.mintTK(contractTK,toMint)
+    console.log(await tx.wait());
+  }
+
+  const handleAddNFT = async ()=>{
+    const addressAcc = String(JSON.parse(localStorage.conn).address);
+    const signer = (await connectMM())[1];
+    const contractNFT = await createContractNFT(signer);
+    console.log(contractNFT);
+    // adicionar condicional para se uriTxt for null
+    const tx = await handleTx.mintNFT(contractNFT, addressAcc, uriTxt)
+    console.log(await tx.wait());
+  }
+  ////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////// 
+  ////////////////////////////////////////////////////////////////////////////////
   return (
       <>
           <span>Address: </span><input type="text" placeholder={JSON.parse(localStorage.getItem('conn')).address} name="" id="" disabled />
@@ -43,25 +77,11 @@ export default function ConnectedProfile ({setConnected}) {
               if (localStorage.balance) setBalance(localStorage.balance);
           }}></span>{balance}<span> MATK </span>
           <button onClick={async () => { 
-              const addressAcc = String(JSON.parse(localStorage.conn).address);
-              const contractTK = await createContractTK(await connectMM());
-              console.log(contractTK);
-              handleTx.getBalance(contractTK,addressAcc)
-              .then((res)=>{
-                  localStorage.setItem("balance",res);
-                  console.log("successful query");
-                  setBalance(res);
-              })
-              .catch(err => +console.log("ERROR DURING TX: ", err))
+            handleRefreshBalance();
            }}>Refresh Balance</button>
           <br />
           <input type="number"  value={toMint} onChange={(e)=>{setToMint(e.target.value)}} /><button onClick={async ()=>{
-            const signer = (await connectMM())[1];
-            const contractTK = await createContractTK([signer]);
-            console.log(signer);
-            console.log(contractTK);
-            const tx = await handleTx.mintTK(contractTK,toMint)
-            console.log(await tx.wait());
+            handleMintTK();
           }}>MintTK</button>
           <br />
           <input type="text" value={uriTxt} onChange={(e)=>{
@@ -70,13 +90,7 @@ export default function ConnectedProfile ({setConnected}) {
             // console.log(uriTxt);
           }}/>
           <button onClick={async () => { 
-            const addressAcc = String(JSON.parse(localStorage.conn).address);
-            const signer = (await connectMM())[1];
-            const contractNFT = await createContractNFT(signer);
-            console.log(contractNFT);
-            const tx = await handleTx.mintNFT(contractNFT, addressAcc, uriTxt)
-            console.log(await tx.wait());
-            
+            handleAddNFT();
           }}>Add</button>
           <br />
           <button onClick={async ()=>{
@@ -84,7 +98,7 @@ export default function ConnectedProfile ({setConnected}) {
             setConnected(0);
           }}>logout</button>
           <br />
-          <span>My Collection:</span> <button onClick={async ()=>{handleGetNft()}}>see</button>
+          <span>My Collection:</span> <button onClick={handleGetNft}>see</button>
           <br />
           <button onClick={()=>{console.log(nft)}}>test</button>
           <div className="hold-test-blocks">
