64a3c51217108745f35f5725dd8e01f093a65f7f
isacpxc <isacpxc@gmail.com>
2025-04-21 16:54:50 -0300

    update

--- src/util/contracts.js
+++ src/util/contracts.js
@@ -1,2 +1,2 @@
 export const TKaddr = "0xd7D3104262c1d44ca27aae1391598517a18AB400";
-export const NFTaddr = "0x32336c78C9AAaf5F4E7eB73C3A3b0b20C086111c";
+export const NFTaddr = "0xF4Ab0600D5fB5Fba8e4c22a2d19fD81cB20AD9f9";
