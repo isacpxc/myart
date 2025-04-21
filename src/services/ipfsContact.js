57e7407ad7117d827286e2ca39211555c2c694db
isacpxc <isacpxc@gmail.com>
2025-04-21 18:17:12 -0300

    feat: working retrieval, but with problems depending on gateway

--- src/services/ipfsContact.js
+++ src/services/ipfsContact.js
@@ -1,6 +1,9 @@
 export const getInfoFromCID = async (cid) => {
   // const res = await fetch(`ipfs://${cid}`, { mode: "no-cors" });
   console.log("cid: ", cid);
-  const res = await fetch(`https://ipfs.io/ipfs/${cid}/`);
+  // const res = await fetch(`https://w3s.link/ipfs/${cid}`); // works
+  // const res = await fetch(`https://dweb.link/ipfs/${cid}`); // works
+  const res = await fetch(`https://ipfs.io/ipfs/${cid}`); // works
+  // https://${cid}.ipfs.dweb.link/
   console.log(await res.json());
 };
