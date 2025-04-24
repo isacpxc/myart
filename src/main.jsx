cef8d0b76c179f20c384643fa2717874b57e1ef7
isacpxc <isacpxc@gmail.com>
2025-04-24 18:12:18 -0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './nft-block.css'
 import App from './App.jsx'
 import Profile from "./Profile.jsx"
 import Settings from "./Settings.jsx"
